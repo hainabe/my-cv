0563d63bbe10c7fa76e9ef36bfa07435bb620f85
hainabe <gsds.sousa@gmail.com>
2018-09-05 14:58:45 +0200

    #002 components structure, almost there!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { WorkComponent } from './work/work.component';
+import { ProfileComponent } from './profile/profile.component';
+import { JobsComponent } from './jobs/jobs.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { HobbiesComponent } from './hobbies/hobbies.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    IntroductionComponent,
+    WorkComponent,
+    ProfileComponent,
+    JobsComponent,
+    SkillsComponent,
+    EducationComponent,
+    HobbiesComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
